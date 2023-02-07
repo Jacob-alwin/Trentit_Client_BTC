b1157413a116c9b5a862e4b57e2a99f61bb45936
Jacob-alwin <alwinjacobjoy@gmail.com>
2023-02-08 02:50:24 +0530

    #2

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -3,8 +3,69 @@ import styles from "@/styles/Details.module.scss";
 import { motion } from "framer-motion";
 // import Carousel3d from "../../../common/Carousel3d";
 // import RS from "../../images/Svg/Rs.svg";
+const data = {
+  success: true,
+  message: "Product fetched",
+  data: {
+    image: {
+      key: "62948dcf-1d91-4982-9d9f-d43b62d439ea.jpeg",
+      mimetype: "image/jpeg",
+    },
+    _id: "63e21b23ca8ce7cc8d1fde74",
+    code: "TTO101",
+    name: "Iphone 13 Pro",
+    information: "Secure phone",
+    originalPrice: 100000,
+    purchasePrice: 10000,
+    salePrice: 10000,
+    grade: "OpenBox",
+    quantity: 2,
+    images: [
+      {
+        key: "c46bd3c5-fa61-4968-be9f-8747577fb137.jpeg",
+        mimetype: "image/jpeg",
+        _id: "63e26274e070cd8b72552a5d",
+      },
+      {
+        key: "1a45c67a-1f34-4592-aee1-388e161a68aa.png",
+        mimetype: "image/png",
+        _id: "63e26274e070cd8b72552a5e",
+      },
+    ],
+    specifications: [
+      {
+        key: "brand",
+        value: "Apple",
+        type: "text",
+        _id: "63e26274e070cd8b72552a5f",
+      },
+      {
+        key: "model",
+        value: "13 Pro Ultra",
+        type: "text",
+        _id: "63e26274e070cd8b72552a60",
+      },
+    ],
+    visibility: "Show",
+    status: "Active",
+    isDeleted: false,
+    createdAt: "2023-02-07T09:34:27.759Z",
+    updatedAt: "2023-02-07T14:38:44.858Z",
+    __v: 2,
+  },
+};
 
 function details() {
+  const [available, setAvailable] = useState(0);
+
+  function handleAdd() {
+    if (available < data.data.quantity) {
+      setAvailable(available + 1);
+      alert("added");
+    } else {
+      alert("Limted Stock");
+    }
+  }
   return (
     <Fragment>
       <div className={styles.LapDetails}>
@@ -12,49 +73,45 @@ function details() {
         <div className={styles.Container + " d-flex justify-content-between"}>
           <div className={styles.Slide}>
             <div className={styles.ShortDes + " "}>
-              <p>ijijijijijij</p>
+              <p>{data.data.information}</p>
             </div>
             <div className={styles.LongDes}>
               <div className={styles.Configurations}>
                 <h1>General Information</h1>
                 <ul>
-                  <li className="d-flex justify-content-between m-2">
-                    <h2>RAM</h2>
-                    <h2>4GB</h2>
-                  </li>
-                  <li className="d-flex justify-content-between m-2">
-                    <h2>RAM</h2>
-                    <h2>4GB</h2>
-                  </li>
-                  <li className="d-flex justify-content-between m-2">
-                    <h2>RAM</h2>
-                    <h2>4GB</h2>
-                  </li>
-                  <li className="d-flex justify-content-between m-2">
-                    <h2>RAM</h2>
-                    <h2>4GB</h2>
-                  </li>
-                  <li className="d-flex justify-content-between m-2">
-                    <h2>RAM</h2>
-                    <h2>4GB</h2>
-                  </li>
+                  {data.data.specifications.map((spec, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="d-flex justify-content-between m-2"
+                      >
+                        <h2>{spec.key}</h2>
+                        <h2>{spec.value}</h2>
+                      </li>
+                    );
+                  })}
                 </ul>
               </div>
             </div>
           </div>
           <div className={styles.Details + " "}>
             <div className={styles.ShortInfo}>
-              <h1>Bike 2022</h1>
+              <h1>{data.data.name}</h1>
 
               <h4>
-                12/22/2222
+                <span onClick={() => handleAdd()}>+</span>
+                {available}
+                <span>-</span>
+
                 <p>Auction Date</p>
               </h4>
               <li>
-                <h2>₹ 200000</h2>
+                <h2>{data.data.grade}</h2>
               </li>
-              <h5>Banglore</h5>
-              <h5>Karnataka</h5>
+              <h5>
+                <del>₹ {data.data.originalPrice}</del>
+              </h5>
+              <h5>₹ {data.data.purchasePrice}</h5>
 
               <motion.div className={styles.favourite + " d-flex m-2 "}>
                 <motion.i
@@ -65,9 +122,7 @@ function details() {
               </motion.div>
             </div>
             <div className={styles.SellerInfo}>
-              <button className="btn btn-primary">
-                Location in map
-              </button>
+              <button className="btn btn-primary">Location in map</button>
             </div>
             <div className={styles.SellerInfo}>
               <h2>Love</h2>
